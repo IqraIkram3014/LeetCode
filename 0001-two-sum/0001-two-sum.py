5dcb55798980f7618a85ae469eb22f4ce1b62778
IqraIkram3014 <iqrasdesigns@gmail.com>
2026-05-09 14:12:08 +0500

    Time: 4 ms (47.57%), Space: 13.4 MB (15.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,12 +5,35 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        num_idx = {num:index for index, num in enumerate(nums)}   
+        """num_idx = {num:index for index, num in enumerate(nums)}   
         for index, num in enumerate(nums):
             sub = target - num
             if sub in num_idx:
                 if num_idx[sub]!=index:
                     return[index, num_idx[sub]]
-                    break
+                    break"""
+
+        arr = []
+        for index, num in enumerate(nums):
+            arr.append((num, index))
+
+        arr.sort() #[(2,0) (7,1) (11,2) (15,3)] this is (2,0) arr[left] and [0] means tuple's index
+
+        left = 0 
+        right = len(arr) - 1 # 3
+
+        while left < right:
+            curr_sum = arr[left][0] + arr[right][0]
+
+            if curr_sum == target:
+                return(arr[left][1], arr[right][1])
+
+            elif curr_sum < target:
+                left+=1
+            
+            else:
+                right-=1
+
+        
         
                                                         

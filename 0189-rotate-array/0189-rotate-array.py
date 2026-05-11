0b24b3dcc3d8feca61d66cbf5b3ec9b8a6347128
IqraIkram3014 <iqrasdesigns@gmail.com>
2026-05-12 01:56:13 +0500

    Time: 4 ms (68.39%), Space: 22.5 MB (32.24%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def rotate(self, nums, k):
         
-        k %= len(nums)
+        k %= len(nums) # k = 7 % 6 == 1 so k is 1
         nums[:] = nums[-k:] + nums[:-k]
 
         
